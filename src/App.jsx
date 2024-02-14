579bc1ca562a758099d19501f4e03bb22d473320
Khalid Shakeel <khalidshakeel46@gmail.com>
2024-02-14 11:36:25 +0530

    This is the mini Portfolio app, built mostly on React Router

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="home bg-slate-500 text-2xl font-bold text-slate-900 shadow-xl shadow-slate-950 p-9 ">
+        {" "}
+        Mini Portfolio Website
+      </div>
+    </>
+  );
+}
+
+export default App;
